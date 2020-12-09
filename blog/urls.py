3630867c867bd687a7fb05315109fd424a76d109
toshi yokoyama <y-toshi@js3.so-net.ne.jp>
2020-12-09 19:57:08 +0900

    Changed the HTML for the site  djangogirls htmlnyumon second git

--- blog/urls.py
+++ blog/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
